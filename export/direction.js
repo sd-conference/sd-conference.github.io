b18800c57781dda36b7f272b4c35ee3cfe34ac74
Park, Jonghyeon <me@ho9.me>
2019-12-28 09:04:13 +0900

    Add files via upload

---
+++ export/direction.js
@@ -0,0 +1,13 @@
+const route = {
+    forum: ['https://forum.sdok.ga', false],
+    ebl: ['https://ebl.sdok.ga', true],
+    books: ['https://books.sdok.ga', false],
+    fcs: ['https://fcs.sdok.ga', false],
+    fix: ['', false]
+}
+
+function call (target) {
+    if (route[target][1]) {
+        window.location.href = route[target][0]
+    }
+}
